70438e85ad2328c01bb94c7123bfb996b6f6fba2
Aldric Goh <azkg2@cam.ac.uk>
2021-03-08 11:17:59 +0000

    Tidied up navigation controller

--- Test project/controllers/Navigation/Navigation.py
+++ Test project/controllers/Navigation/Navigation.py
@@ -1,125 +1,12 @@
 from controller import Robot, Motor, Keyboard, Compass, GPS
 import numpy as np
 import time
+from functions import *
+from setup import *
 
 print('start')
-#Useful constants
-TIME_STEP = 16
-MAX_SPEED = 3
-
-#ROBOT CONSTANTS
-WHEEL_RADIUS = 5.08
-ROBOT_WIDTH = 18 + 2*WHEEL_RADIUS
-
-# --DO NOT EDIT--
-TURN_RADIUS = (ROBOT_WIDTH-WHEEL_RADIUS+1.11)/2
-# --DO NOT EDIT--
-
-robot = Robot()
-
-#get robot motor devices
-left_wheel = robot.getDevice("left_wheel")
-right_wheel = robot.getDevice("right_wheel")
-left_door = robot.getDevice("left_door")
-right_door = robot.getDevice("right_door")
-
-left_wheel.setVelocity(MAX_SPEED)
-right_wheel.setVelocity(MAX_SPEED)
-    
-#get robot sensor devices
-ds_bottom = robot.getDevice('ds_long')
-ds_top= robot.getDevice('ds_short')
-ls_red = robot.getDevice('ls_red')
-ls_green = robot.getDevice('ls_green')
-gps = robot.getDevice("gps")
-compass = robot.getDevice("compass")
-
-#enable sensors
-ds_bottom.enable(TIME_STEP)
-ds_top.enable(TIME_STEP)
-ls_red.enable(TIME_STEP) 
-ls_green.enable(TIME_STEP)
-gps.enable(TIME_STEP)
-compass.enable(TIME_STEP)
-#Emitter Receiver
 
 timestep = int(robot.getBasicTimeStep())
-
-def robotLocation():
-    coord3d = gps.getValues()
-    coord2d = [coord3d[0],coord3d[2]]
-    return coord2d
- 
-def compassAngle():
-    compassValues = compass.getValues()
-    rad = -np.arctan2(compassValues[0],compassValues[2])
-    return round(rad, 2) #+- sign essential
-    
-def dist(x1,z1,x2,z2):
-    return (x2-x1)**2+(x2-x1)**2
-
-def observe():
-    left_wheel.setPosition(float('inf'))
-    right_wheel.setPosition(float('inf'))
-    left_wheel.setVelocity(-MAX_SPEED)
-    right_wheel.setVelocity(MAX_SPEED)
-    print(compassAngle())
-    print(ds_bottom.getValue())
-    robot.step(1)
-    
-def moveToPosition(position):
-    #moves wheels to position (in rads)
-    left_wheel.setPosition(position)
-    right_wheel.setPosition(position)
-
-def turn():
-    left_wheel.setPosition(float('inf'))
-    right_wheel.setPosition(float('inf'))
-    left_wheel.setVelocity(-MAX_SPEED)
-    right_wheel.setVelocity(MAX_SPEED)
-
-def openDoor():
-    left_door.setPosition(1.57)
-    right_door.setPosition(1.57)
-
-def closeDoor():
-    left_door.setPosition(0)
-    right_door.setPosition(0)
-
-#Turns anti-clockwise by x radians
-def turnRadian(radians):
-    position = (radians*TURN_RADIUS)/(WHEEL_RADIUS)
-    left_wheel.setPosition(-position)
-    right_wheel.setPosition(position)
-    left_wheel.setVelocity(MAX_SPEED)
-    right_wheel.setVelocity(MAX_SPEED)
-    
-
-def checkObstacles(destination):
-    #Destination is coordinates we want to go
-    #TURN +-66 degrees about axis of symmetry of robot
-    #Scan if there are any obstacles to the location desired
-    #Using ds_bottom
-    #If no, return False
-    #If yes, return True
-    #if compass.getValues()
-    turnRadian((11*np.pi)/30)
-    #if 
-    
-def align(direction):
-    currentAngle = compassAngle()
-    while (currentAngle >= direction+(1e-5)) and (currentAngle <= direction-(1e-5)):
-        turn()
-        print(currentAngle)
-        robot.step(1)
-    print('aligned')
-    
-def passive_wait(time):
-    start_time = robot.getTime()
-    while start_time + time > robot.getTime():
-        print(start_time + time)
-        print(robot.getTime())
-        robot.step(1)
 
 while robot.step(timestep) != -1:
     # coord3d = gps.getValues()
@@ -129,8 +16,7 @@ while robot.step(timestep) != -1:
     
     # passive_wait(2)
     # turnRadian(2)
-    observe()
-    
+    align(-0.01)
     # passive_wait(2)
     
     # break
---
+++ Test project/functions.py
@@ -0,0 +1,105 @@
+from controller import Robot, Motor, Keyboard, Compass, GPS
+import numpy as np
+import time
+
+def robotLocation():
+    coord3d = gps.getValues()
+    coord2d = [coord3d[0],coord3d[2]]
+    return coord2d
+ 
+def compassAngle():
+    compassValues = compass.getValues()
+    rad = -np.arctan2(compassValues[0],compassValues[2])
+    return round(rad, 2) #+- sign essential
+    
+def dist(x1,z1,x2,z2):
+    return (x2-x1)**2+(x2-x1)**2
+    
+def dist_to_wall():
+    #Finds what the distance sensor should be reading
+    angle = compassAngle()
+    position = gps.getValues()
+    x, z = position[0], position[1]
+    wallN = abs((1.2-0.01 - x) / np.sin(angle))#0.01 terms as wall has a thickness
+    wallS = abs((-1.2+0.01 - x) / np.sin(angle))
+    wallW = abs((-1.2+0.01 - z) / np.cos(angle))
+    wallE = abs((+1.2-0.01 - z) / np.cos(angle))
+    if (angle>=0 and angle <= np.pi/2):
+        return min([wallN,wallE,1.6])
+    elif (angle>=np.pi/2 and angle <= np.pi):
+        return round(min([wallW,wallN,1.6]), 2)
+    if (angle<0 and angle >= -np.pi/2):
+        return round(min([wallS,wallW,1.6]), 2)
+    elif (angle<=-np.pi/2 and angle > -np.pi):
+        return round(min([wallE,wallS,1.6]), 2)
+    else:
+        return 1
+
+def get_object_position(position,angle,sensordist):
+    #Determines the coordinates of the box
+    x = position[0]
+    z = position[1]
+    return [x+(sensordist+0.025) * np.sin(angle),z+(sensordist+0.025) * np.cos(angle)]#Adds to centroid, may need to be tuned
+
+def checkIfBox():
+    turn()
+    print(ds_bottom.getValue(), dist_to_wall())
+    if ds_bottom.getValue() < dist_to_wall():
+        print('Object found at:' + str(compassAngle()) + str(sensor_to_dist()))
+        robot.step(1)
+    robot.step(1)
+        
+
+def moveToPosition(position):
+    #moves wheels to position (in rads)
+    left_wheel.setPosition(position)
+    right_wheel.setPosition(position)
+
+def turn():
+    left_wheel.setPosition(float('inf'))
+    right_wheel.setPosition(float('inf'))
+    left_wheel.setVelocity(-MAX_SPEED)
+    right_wheel.setVelocity(MAX_SPEED)
+
+def openDoor():
+    left_door.setPosition(1.57)
+    right_door.setPosition(1.57)
+
+def closeDoor():
+    left_door.setPosition(0)
+    right_door.setPosition(0)
+
+#Turns anti-clockwise by x radians
+def turnRadian(radians):
+    position = (radians*TURN_RADIUS)/(WHEEL_RADIUS)
+    left_wheel.setPosition(-position)
+    right_wheel.setPosition(position)
+    left_wheel.setVelocity(MAX_SPEED)
+    right_wheel.setVelocity(MAX_SPEED)
+    
+
+def checkObstacles(destination):
+    #Destination is coordinates we want to go
+    #TURN +-66 degrees about axis of symmetry of robot
+    #Scan if there are any obstacles to the location desired
+    #Using ds_bottom
+    #If no, return False
+    #If yes, return True
+    #if compass.getValues()
+    turnRadian((11*np.pi)/30)
+    #if 
+    
+def align(direction):
+    currentAngle = compassAngle()
+    while currentAngle != direction:
+        turn()
+        print(currentAngle)
+        robot.step(1)
+    print('aligned')
+    
+def passive_wait(time):
+    start_time = robot.getTime()
+    while start_time + time > robot.getTime():
+        print(start_time + time)
+        print(robot.getTime())
+        robot.step(1)
